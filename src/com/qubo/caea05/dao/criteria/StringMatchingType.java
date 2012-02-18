18ab49dfba60f5ed5308cf52be28a3dec81fc4e5
tkQubo <tk.qubo@gmail.com>
2012-02-18 16:31:16 +0900

    StringMatchingType modified.

--- src/com/qubo/caea05/dao/criteria/StringMatchingType.java
+++ src/com/qubo/caea05/dao/criteria/StringMatchingType.java
@@ -6,11 +6,13 @@ package com.qubo.caea05.dao.criteria;
  */
 public enum StringMatchingType {
 	/** 完全一致検索 */
-	Exact,
+	Exact { @Override public String getLabel() { return "完全一致"; } },
 	/** 部分一致検索 */
-	Contains,
+	Contains { @Override public String getLabel() { return "部分一致"; } },
 	/** 前方一致検索 */
-	StartsWith,
+	StartsWith { @Override public String getLabel() { return "前方一致"; } },
 	/** 後方一致検索 */
-	EndsWith,
+	EndsWith { @Override public String getLabel() { return "後方一致"; } },
+	;
+	public abstract String getLabel();
 }
